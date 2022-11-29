7a8384b14ee367996b2f9945f847266d0abd7bfe
Jorge Amaro <inf.amaro.jc@gmail.com>
2022-11-28 21:04:22 -0300

    Entity iniciadas

---
+++ backend/product-api/src/main/java/com/amaro/microsservices/entity/Category.java
@@ -0,0 +1,67 @@
+package com.amaro.microsservices.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id; 
+	private String nome;
+	
+	public Category() {
+	}
+
+	public Category(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Category [id=");
+		builder.append(id);
+		builder.append(", nome=");
+		builder.append(nome);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
---
+++ backend/product-api/src/main/java/com/amaro/microsservices/entity/Product.java
@@ -0,0 +1,124 @@
+package com.amaro.microsservices.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Product implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+ 
+	private Long id;
+	private String nome;
+	private Float preco;
+	private String descricao;
+	private String productIdentifier;
+	private Category category;
+	
+	public Product() {
+
+	}
+
+	public Product(Long id, String nome, Float preco, String descricao, String productIdentifier) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+		this.productIdentifier = productIdentifier;
+	}
+
+	public Product(Long id, String nome, Float preco, String descricao, String productIdentifier, Category category) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+		this.productIdentifier = productIdentifier;
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [id=");
+		builder.append(id);
+		builder.append(", nome=");
+		builder.append(nome);
+		builder.append(", preco=");
+		builder.append(preco);
+		builder.append(", descricao=");
+		builder.append(descricao);
+		builder.append(", productIdentifier=");
+		builder.append(productIdentifier);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getProductIdentifier() {
+		return productIdentifier;
+	}
+
+	public void setProductIdentifier(String productIdentifier) {
+		this.productIdentifier = productIdentifier;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+}
