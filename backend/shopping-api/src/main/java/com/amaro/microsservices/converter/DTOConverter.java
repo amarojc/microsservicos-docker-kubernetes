3902e546f803a61bc82b7534e5a16325b210841d
Jorge Amaro <inf.amaro.jc@gmail.com>
2022-12-13 16:55:19 -0300

    DTOConverter Shop

---
+++ backend/shopping-api/src/main/java/com/amaro/microsservices/converter/DTOConverter.java
@@ -0,0 +1,29 @@
+package com.amaro.microsservices.converter;
+
+import java.util.stream.Collectors;
+
+import com.amaro.microsservices.dto.ItemDTO;
+import com.amaro.microsservices.dto.ShopDTO;
+import com.amaro.microsservices.entity.Item;
+import com.amaro.microsservices.entity.Shop;
+
+public class DTOConverter {
+	
+	public static ItemDTO convert(Item item) {
+		ItemDTO itemDTO = new ItemDTO();
+		itemDTO.setProductIdentifier(item.getProductIdentifier());
+		itemDTO.setPrice(item.getPrice());
+		return itemDTO;
+	}
+
+	public static ShopDTO convert(Shop shop) {
+		ShopDTO shopDTO = new ShopDTO();
+		shopDTO.setId(shop.getId());
+		shopDTO.setUserIdentifier(shop.getUserIdentifier());
+		shopDTO.setTotal(shop.getTotal());
+		shopDTO.setDate(shop.getDate());
+		shopDTO.setItens(shop.getItens().stream().map(DTOConverter::convert).collect(Collectors.toList()));
+		
+		return shopDTO;
+	}
+}
